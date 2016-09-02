333e92581af874ad77f295d9abec0a55ebfa17e3
David Hill <DavidPH55@verizon.net>
2016-09-02 15:13:09 -0400

    GL: Do not move temporaries

--- GL/Color.hpp
+++ GL/Color.hpp
@@ -27,31 +27,31 @@ namespace Doom
       //
       // Color
       //
-      
+
       class Color
       {
       public:
          Color(float r_, float g_, float b_, float a_ = 1.0f) : r{r_}, g{g_}, b{b_}, a{a_} {}
          Color(double r_, double g_, double b_, double a_ = 1.0) : r{r_}, g{g_}, b{b_}, a{a_} {}
-         
+
          static Color FromHSV(double h, double s, double v, double a = 1.0);
          static Color Interpolate(Color const &a, Color const &b, double amt);
-         
+
          Color &fromHSV(double h, double s, double v, double aa = 1.0)
          {
-            return *this = std::move(Color::FromHSV(h, s, v, aa));
+            return *this = Color::FromHSV(h, s, v, aa);
          }
-         
+
          Color &interpolate(Color const &other, double amt)
          {
             r = ((1.0 - amt) * r) + (amt * other.r);
             g = ((1.0 - amt) * g) + (amt * other.g);
             b = ((1.0 - amt) * b) + (amt * other.b);
             a = ((1.0 - amt) * a) + (amt * other.a);
-            
+
             return *this;
          }
-         
+
          double r = 1.0;
          double g = 1.0;
          double b = 1.0;
