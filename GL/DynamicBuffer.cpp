f0b392287c22d5fa9aa6c1cb1c54d2480201dc85
Graham <marrub@greyserv.net>
2016-12-10 20:43:22 -0500

    GL: Add a destructor to DynamicBuffer

--- GL/DynamicBuffer.cpp
+++ GL/DynamicBuffer.cpp
@@ -12,6 +12,16 @@
 
 #include "GL/DynamicBuffer.hpp"
 
+#define GenLayoutPoint(type, fnName, name) \
+   if(layout.name##Len) \
+   { \
+      glEnableClientState(type); \
+      gl##fnName##Pointer(layout.name##Len, GL_FLOAT, layout.size, \
+         reinterpret_cast<GLvoid *>(sizeof(float) * layout.name##Pos)); \
+   } \
+   else \
+      glDisableClientState(type)
+
 
 //----------------------------------------------------------------------------|
 // Extern Objects                                                             |
@@ -19,20 +29,20 @@
 
 namespace DGE::GL
 {
-   VertexLayout VertexXY::Layout{
-      /* size           */ sizeof(VertexXY),
-      /* pntLen, pntPos */     2, 0,
-      /* texLen, texPos */     0, 0,
-      /* colLen, colPos */     0, 0,
-      /* nrmUse, nrmPos */ false, 0
+   VertexLayout const VertexXY::Layout{
+      /* Size    */ sizeof(VertexXY),
+      /* Point   */     2, 0,
+      /* Texture */     0, 0,
+      /* Colorn  */     0, 0,
+      /* Normal  */ false, 0
    };
 
-   VertexLayout VertexXYUV::Layout{
-      /* size           */ sizeof(VertexXYUV),
-      /* pntLen, pntPos */     2, 0,
-      /* texLen, texPos */     2, 2,
-      /* colLen, colPos */     0, 0,
-      /* nrmUse, nrmPos */ false, 0
+   VertexLayout const VertexXYUV::Layout{
+      /* Size    */ sizeof(VertexXYUV),
+      /* Point   */     2, 0,
+      /* Texture */     2, 2,
+      /* Colorn  */     0, 0,
+      /* Normal  */ false, 0
    };
 }
 
@@ -44,9 +54,16 @@ namespace DGE::GL
 namespace DGE::GL
 {
    //
+   // DynamicBuffer destructor
+   //
+   DynamicBuffer::~DynamicBuffer()
+   {
+      if(glIsBuffer(buffer)) glDeleteBuffers(1, &buffer);
+   }
+
+   //
    // DynamicBuffer::bind
    //
-
    void DynamicBuffer::bind()
    {
       glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -55,7 +72,6 @@ namespace DGE::GL
    //
    // DynamicBuffer::draw
    //
-
    void DynamicBuffer::draw()
    {
       setupPointers();
@@ -65,37 +81,17 @@ namespace DGE::GL
    //
    // DynamicBuffer::setupPointers
    //
-
    void DynamicBuffer::setupPointers()
    {
-      if(layout.pntLen)
-      {
-         glEnableClientState(GL_VERTEX_ARRAY);
-         glVertexPointer(layout.pntLen, GL_FLOAT, layout.size, (GLvoid *)(sizeof(float) * layout.pntPos));
-      }
-      else
-         glDisableClientState(GL_VERTEX_ARRAY);
-
-      if(layout.texLen)
-      {
-         glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-         glTexCoordPointer(layout.texLen, GL_FLOAT, layout.size, (GLvoid *)(sizeof(float) * layout.texPos));
-      }
-      else
-         glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-
-      if(layout.colLen)
-      {
-         glEnableClientState(GL_COLOR_ARRAY);
-         glColorPointer(layout.colLen, GL_FLOAT, layout.size, (GLvoid *)(sizeof(float) * layout.colPos));
-      }
-      else
-         glDisableClientState(GL_COLOR_ARRAY);
+      GenLayoutPoint(GL_VERTEX_ARRAY,        Vertex,   pnt);
+      GenLayoutPoint(GL_TEXTURE_COORD_ARRAY, TexCoord, tex);
+      GenLayoutPoint(GL_COLOR_ARRAY,         Color,    col);
 
       if(layout.nrmUse)
       {
          glEnableClientState(GL_NORMAL_ARRAY);
-         glNormalPointer(GL_FLOAT, layout.size, (GLvoid *)(sizeof(float) * layout.nrmPos));
+         glNormalPointer(GL_FLOAT, layout.size,
+            reinterpret_cast<GLvoid *>(sizeof(float) * layout.nrmPos));
       }
       else
          glDisableClientState(GL_NORMAL_ARRAY);
@@ -104,11 +100,9 @@ namespace DGE::GL
    //
    // DynamicBuffer::setupData
    //
-
    void DynamicBuffer::setupData(std::size_t arraysize, void *data, GLenum type)
    {
-      if(!buffer)
-         glGenBuffers(1, &buffer);
+      if(!glIsBuffer(buffer)) glGenBuffers(1, &buffer);
 
       bind();
 
--- GL/DynamicBuffer.hpp
+++ GL/DynamicBuffer.hpp
@@ -14,6 +14,7 @@
 #define DGE__GL__DynamicBuffer_H__
 
 #include "GL/OpenGL2.1.h"
+
 #include <cstddef>
 
 
@@ -28,59 +29,51 @@ namespace DGE::GL
    //
    // VertexLayout
    //
-
    struct VertexLayout
    {
       GLsizei size;
       GLsizei pntLen, pntPos; // Point
       GLsizei texLen, texPos; // Texture
       GLsizei colLen, colPos; // Color
-      GLsizei         nrmPos; // Normal
       bool    nrmUse;
+      GLsizei nrmPos; // Normal
    };
 
    //
    // VertexXY
    //
-
    struct VertexXY
    {
       float x, y;
 
-      static VertexLayout Layout;
+      static VertexLayout const Layout;
    };
 
    //
    // VertexXYUV
    //
-
    struct VertexXYUV
    {
       float x, y, u, v;
 
-      static VertexLayout Layout;
+      static VertexLayout const Layout;
    };
 
    //
    // DynamicBuffer
    //
-
    class DynamicBuffer
    {
    public:
       DynamicBuffer() = delete;
-      DynamicBuffer(DynamicBuffer const &other) = delete;
-
       DynamicBuffer(VertexLayout layout_, GLenum drawtype_ = GL_TRIANGLES) :
-         buffer{},
-         size{},
-         layout{layout_},
-         drawtype{drawtype_}
-      {
-      }
+         buffer{}, size{}, layout{layout_}, drawtype{drawtype_} {}
+      DynamicBuffer(DynamicBuffer const &) = delete;
+      DynamicBuffer(DynamicBuffer &&) = default;
+      ~DynamicBuffer();
 
       void bind();
-      void bindAndDraw() { bind(); draw(); }
+      void bindAndDraw() {bind(); draw();}
       void draw();
       void setupData(std::size_t arraysize, void *data, GLenum type);
       void setupPointers();
