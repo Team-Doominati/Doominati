7a60d6bbd63b92ff4398c214c449e887eb6978a2
Graham <marrub@greyserv.net>
2017-02-22 14:33:57 -0500

    AL: Add Seek and Tell callbacks for Vorbis decoding
    AL: Clamp values read from Vorbis streams

--- AL/Sound/Vorbis.cpp
+++ AL/Sound/Vorbis.cpp
@@ -19,6 +19,8 @@
 #include <vorbis/vorbisfile.h>
 
 #include <memory>
+#include <cstring>
+#include <algorithm>
 
 
 //----------------------------------------------------------------------------|
@@ -38,13 +40,19 @@ namespace DGE::AL
          file{file_},
          vorbis{}
       {
-         ov_open_callbacks(this, &vorbis, nullptr, 0, {Read});
+         ov_open_callbacks(this, &vorbis, nullptr, 0, {Read, Seek, nullptr, Tell});
 
          auto inf = ov_info(&vorbis, -1);
 
          stereo   = inf->channels > 1;
          samprate = inf->rate;
-         samples  = ov_pcm_total(&vorbis, -1);
+
+         auto total = ov_pcm_total(&vorbis, -1);
+
+         if(total == OV_EINVAL)
+            throw SoundLoaderError("invalid stream length");
+         else
+            samples = total;
       }
 
       virtual ~SoundLoader_Vorbis()
@@ -62,11 +70,15 @@ namespace DGE::AL
       static std::size_t Read(void *out, std::size_t size, std::size_t nmemb,
          void *userdata);
 
+      static int Seek(void *userdata, ogg_int64_t offset, int whence);
+
+      static long Tell(void *userdata);
+
       bool        stereo;
       std::size_t samprate;
       std::size_t samples;
 
-      std::size_t    pos;
+      ogg_int64_t    pos;
       FS::File      *file;
       OggVorbis_File vorbis;
    };
@@ -84,11 +96,11 @@ namespace DGE::AL
    //
    void SoundLoader_Vorbis::data(SoundSample *buf)
    {
-      float **buffer;
-      int frame = 0;
-      int channels = stereo ? 2 : 1;
+      float **buffer   = nullptr;
+      int     frame    = 0;
+      int     channels = stereo ? 2 : 1;
 
-      for(;;)
+      for(std::size_t iter = 0; iter < samples;)
       {
          long blocksize = ov_read_float(&vorbis, &buffer, 1024, &frame);
 
@@ -96,7 +108,10 @@ namespace DGE::AL
          {
             for(long i = 0; i < blocksize; i++)
                for(int j = 0; j < channels; j++)
-                  *buf++ = static_cast<SoundSample>(buffer[j][i] * 32767);
+            {
+               float clamped = std::clamp(buffer[j][i], -1.0f, 1.0f);
+               buf[iter++] = static_cast<SoundSample>(clamped * 32767);
+            }
          }
          else if(blocksize < 0)
             throw SoundLoaderError("invalid vorbis stream");
@@ -128,6 +143,35 @@ namespace DGE::AL
       }
 
       return read;
+   }
+
+   //
+   // SoundLoader_Vorbis::Seek
+   //
+   int SoundLoader_Vorbis::Seek(void *userdata, ogg_int64_t offset, int whence)
+   {
+      auto self = static_cast<SoundLoader_Vorbis *>(userdata);
+
+      switch(whence)
+      {
+      default: return -1;
+      case SEEK_END: self->pos  = self->file->size + offset; return 0;
+      case SEEK_SET: self->pos  = offset; return 0;
+      case SEEK_CUR: self->pos += offset; return 0;
+      }
+   }
+
+   //
+   // SoundLoader_Vorbis::Tell
+   //
+   long SoundLoader_Vorbis::Tell(void *userdata)
+   {
+      auto self = static_cast<SoundLoader_Vorbis *>(userdata);
+
+      if(self->pos > self->file->size)
+         return EOF;
+      else
+         return self->pos;
    }
 
    //
