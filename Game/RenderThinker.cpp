d7d8df7990e07924cb34dfce307b7ccbda151fe9
David Hill <DavidPH55@verizon.net>
2017-09-13 20:42:27 -0400

    Game: Add DGE_RenderThinker_Create native

--- Game/RenderThinker.cpp
+++ Game/RenderThinker.cpp
@@ -12,6 +12,9 @@
 
 #include "Game/RenderThinker.hpp"
 
+#include "Code/Native.hpp"
+#include "Code/Task.hpp"
+
 
 //----------------------------------------------------------------------------|
 // Extern Functions                                                           |
@@ -20,6 +23,25 @@
 namespace DGE::Game
 {
    DGE_Game_ThinkerImplement(RenderThinker);
+}
+
+
+//----------------------------------------------------------------------------|
+// Natives                                                                    |
+//
+
+namespace DGE::Game
+{
+   //
+   // unsigned DGE_RenderThinker_Create(unsigned ext)
+   //
+   DGE_Code_NativeDefn(DGE_RenderThinker_Create)
+   {
+      std::size_t ext = argv[0];
+
+      task->dataStk.push((new(ext) RenderThinker)->id);
+      return false;
+   }
 }
 
 // EOF
